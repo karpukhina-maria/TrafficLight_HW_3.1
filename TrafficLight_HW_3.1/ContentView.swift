6fafde5729def632d1826ed089cdc934a4dbbe55
Maria Karpukhina <myshka_n@mail.ru>
2021-10-27 08:37:02 +0300

    Initial Commit

---
+++ TrafficLight_HW_3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLight_HW_3.1
+//
+//  Created by Mary Jane on 27.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLight_HW_3.1/TrafficLight_HW_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_HW_3_1App.swift
+//  TrafficLight_HW_3.1
+//
+//  Created by Mary Jane on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_HW_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
